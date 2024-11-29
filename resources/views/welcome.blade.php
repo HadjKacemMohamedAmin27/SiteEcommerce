f2eff0c36c02b2abb83d00f05eeb513bd6860c2d
Mohamed Amin Hadj Kacem <medaminhk27@gmail.com>
2024-11-29 10:35:53 +0100

    final commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mon application Laravel</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecurerequests">
+        <link href="{{ secure_asset('frontend/assets/index-CPXlk9qb.css') }}"rel="stylesheet">
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="{{ secure_asset('frontend/assets/index-DdMtEjaC.js')}}"></script>
+    </body>
+</html>
